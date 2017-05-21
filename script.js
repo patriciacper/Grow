ae6a4f9754a0599c2022c33bf91d030d4256fdaf
patriciacper <patriciacospereira@gmail.com>
2017-05-21 16:43:27 +0100

    JavaScript

---
+++ script.js
@@ -0,0 +1,98 @@
+var registerListeners = function () {
+    $(".opt").on("click", function () {
+        var option = $(this).attr("data-id");
+        var page = $(this).parents(".page").attr("id");
+
+        $(".b-" + page).find("p.zero").hide();
+
+        activeOptions = $(".active-opt");
+
+        if (activeOptions.length > 0) {
+            activeOptions.removeClass("active-opt").addClass("opt1");
+        }
+
+        $(".b-" + page).find("." + option).removeClass("opt1").addClass("active-opt");
+
+        $(this).css("opacity", 1);
+        $(this).siblings().css("opacity", 0.5);
+    });
+
+    $(".next").on("click", function () {
+        var visiblePage = $(".page:visible"),
+            visibleFooter = $(visiblePage).attr("id");
+
+        $(visiblePage).hide();
+        $(visiblePage).next().show();
+        $(".previous").show();
+
+        if (visibleFooter !== "fourth") {
+            $(".b-" + visibleFooter).hide();
+            $(".b-" + visibleFooter).next().show();
+        } else {
+            $(".b-final").show();
+            $(".next").hide();
+            $(".close").show();
+        }
+    });
+
+    $(".previous").on("click", function () {
+        var visiblePage = $(".page:visible"),
+            visibleFooter = $(visiblePage).attr("id");
+
+        $(visiblePage).hide();
+        $(visiblePage).prev().show();
+
+        if (visibleFooter === "final") {
+            $(".b-" + visibleFooter).hide();
+            $(".next").show();
+            $(".close").hide();
+        } else if (visibleFooter === "fourth") {
+            $(".b-third").show();
+        } else if (visibleFooter === "second") {
+            $(".previous").hide();
+            $(".b-" + visibleFooter).hide();
+            $(".b-" + visibleFooter).prev().show();
+        } else {
+            $(".b-" + visibleFooter).hide();
+            $(".b-" + visibleFooter).prev().show();
+        }
+
+    });
+
+    $(".close").on("click", function () {
+        $(".modal").hide();
+        $(".result").hide();
+        $(".b-final").hide();
+        $(".previous").hide();
+        $(".close").hide();
+        $("footer").hide();
+        $(".fourth").find(".round").css("background", "none");
+        $(".fourth").find("p").hide();
+        $("#home").show();
+    });
+
+    $("button").on("click", function () {
+        $("#home").hide();
+        $(".modal").show();
+        $("footer").show();
+        $(".intro").show();
+        $(".b-intro").show();
+        $(".next").show();
+    });
+
+    $(".round").on("click", function () {
+        var selectedLevel = $(this).attr("data-id"),
+            selectedType = $(this).parent().attr("class");
+
+        $(this).css("background-color", "green");
+        $(this).nextAll(".round").css("background-color", "green");
+        $(this).prevAll(".round").css("background", "none");
+
+        $("p." + selectedType).hide();
+        $("." + selectedType + "." + selectedLevel).show();
+    });
+}
+
+$(document).ready(function () {
+    registerListeners();
+});
